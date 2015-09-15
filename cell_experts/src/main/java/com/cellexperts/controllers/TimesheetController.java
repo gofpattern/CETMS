0f2fa90d4dbbba5f83ebe0d6b3abe3d185faf03a
gofpattern <gofpattern@gmail.com>
2015-09-15 12:07:39 -0400

    trying with database

--- cell_experts/src/main/java/com/cellexperts/controllers/TimesheetController.java
+++ cell_experts/src/main/java/com/cellexperts/controllers/TimesheetController.java
@@ -1,5 +1,14 @@
 package com.cellexperts.controllers;
 
+import java.util.HashSet;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -12,9 +21,12 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.cellexperts.beans.Employee;
 import com.cellexperts.db.hbm.Employees;
+import com.cellexperts.db.hbm.Store;
 
 @Controller
 public class TimesheetController {
+	
+	static SessionFactory factory = getSessionFactory();
 	
 	@RequestMapping(value = "/timesheet", method = RequestMethod.GET)
 	public String helloWorld(Model model, @ModelAttribute("employee") Employee employee, BindingResult result) {		
@@ -27,8 +39,70 @@ public class TimesheetController {
 	   System.out.println(employee.getUser());
 	   System.out.println(employee.getPwd());
 	    
-	      
+	   addEmployee(employee.getUser());
 	      return "Timesheet";
 	 }
+	 
+	 
+
+
+		
+
+		
+
+		private static SessionFactory getSessionFactory()
+				throws ExceptionInInitializerError {
+			SessionFactory factory;
+			try {
+				// loads configuration and mappings
+		        Configuration configuration = new Configuration().configure();
+		        ServiceRegistryBuilder registry = new ServiceRegistryBuilder();
+		        registry.applySettings(configuration.getProperties());
+		        ServiceRegistry serviceRegistry = registry.buildServiceRegistry();
+		         
+		        // builds a session factory from the service registry
+		        factory = configuration.buildSessionFactory(serviceRegistry);
+			} catch (Throwable ex) {
+				System.err.println("Failed to create sessionFactory object." + ex);
+				throw new ExceptionInInitializerError(ex);
+			}
+			return factory;
+		}
+
+		public static Integer addEmployee(String login) {
+			Session session = factory.openSession();
+			Transaction tx = null;
+			Integer employeeID = null;
+			Integer storeID = null;
+			try {
+				tx = session.beginTransaction();
+				Employees emp = new Employees("aaqib", "rehman", null, null,
+						login, null, "31 N Street, Allentown",
+						null, null);
+				//assign employees to store
+				HashSet<Store> storeSet = new HashSet<Store>();
+				
+				Store store = new Store("Reading","Cell Expert Systems",null);
+				storeSet.add(store);
+				emp.setStores(storeSet);
+				//indpendent tables
+				employeeID = (Integer) session.save(emp);
+				storeID = (Integer) session.save(store);
+				tx.commit();
+				///////////////////
+				
+				
+				
+			} catch (HibernateException e) {
+				if (tx != null)
+					tx.rollback();
+				e.printStackTrace();
+			} finally {
+				session.close();
+			}
+			return employeeID;
+		}
+
+
 
 }
